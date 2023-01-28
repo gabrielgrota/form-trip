8d7aadeab075a7022b27d3e3d9e4c5a7a377d82d
gabrielgrota <biel.grohaha@gmail.com>
2023-01-28 18:26:11 -0300

    Function .filter() add

--- countries.js
+++ countries.js
@@ -1,132 +1,181 @@
-let arg = {
-    flag: 'flags/arg.png',
-    airportCode: 'EZE',
-    airportName: 'Ezeiza Airport',
-    city: 'Buenos Aires, Argentina'
-};
-
-let aus = {
-    flag: 'flags/aus.png',
-    airportCode: 'SYD',
-    airportName: 'Sydney Kingsford Smith Airport',
-    city: 'Sydney, Australia'
-};
-
-let aut = {
-    flag: 'flags/aut.png',
-    airportCode: 'VIE',
-    airportName: 'Vienna Airport',
-    city: 'Vienna, Austria'
-};
-
-let bel = {
-    flag: 'flags/bel.png',
-    airportCode: 'BRU',
-    airportName: 'Brussels Airport',
-    city: 'Brussels, Belgium'
-};
-
-let bol = {
-    flag: 'flags/bol.png',
-    airportCode: 'VVI',
-    airportName: 'Viru Viru Airport',
-    city: 'Santa Cruz de la Sierra, Bolivia'
-};
-
-let bra = { 
-    flag: 'flags/bra.png',
-    airportCode: 'GRU',
-    airportName: 'Guarulhos Airport',
-    city: 'São Paulo, Brazil'
-};
-
-let bwa = {
-    flag: 'flags/bwa.png',
-    airportCode: 'GBE',
-    airportName: 'Sir Seretse Khama Airport',
-    city: 'Gaborone, Botswana'
-};
-
-let can = {
-    flag: 'flags/can.png',
-    airportCode: 'YYZ',
-    airportName: 'Lester B. Pearson Airport',
-    city: 'Ontario, Canada'
-};
-
-let chl = {
-    flag: 'flags/chl.png',
-    airportCode: 'SCL',
-    airportName: 'Arturo Merino Benítez Airport',
-    city: 'Santiago, Chile'
-};
-
-let chn = {
-    flag: 'flags/chn.png',
-    airportCode: 'PEK',
-    airportName: 'Beijing Capital Airport',
-    city: 'Beijing, China'
-};
-
-let cym = {
-    flag: 'flags/cym.png',
-    airportCode: 'GCM',
-    airportName: 'Owen Roberts Airport',
-    city: 'George Town, Cayman Islands'
-};
-
-let cze = {
-    flag: 'flags/cze.png',
-    airportCode: 'PRG',
-    airportName: 'Václav Havel Airport',
-    city: 'Prague, Czech Republic'
-};
-
-let eng = {
-    flag: 'flags/eng.png',
-    airportCode: 'LHR',
-    airportName: 'Heathrow Airport',
-    city: 'London, England, UK'
-};
-
-let idn = {
-    flag: 'flags/idn.png',
-    airportCode: 'CGK',
-    airportName: 'Soekarno–Hatta Airport',
-    city: 'Jakarta, Indonesia'
-};
-
-let jpn = {
-    flag: 'flags/jpn.png',
-    airportCode: 'NRT',
-    airportName: 'Narita Airport',
-    city: 'Narita, Chiba, Japan'
-};
-
-let kor = {
-    flag: 'flags/kor.png',
-    airportCode: 'ICN',
-    airportName: 'Incheon Airport',
-    city: 'Jung District, South Korea'
-};
-
-let nzl = {
-    flag: 'flags/nzl.png',
-    airportCode: 'AKL',
-    airportName: 'Auckland Airport',
-    city: 'Auckland, New Zealand'
-};
-
-let swe = {
-    flag: 'flags/swe.png',
-    airportCode: 'ARN',
-    airportName: 'Stockholm Arlanda Airport',
-    city: 'Stockholm, Sweden'
-};
-
-let usa = {
-    flag: 'flags/usa.png',
-    airportCode: 'JFK',
-    airportName: 'John F. Kennedy Airport',
-    city: 'New York, U.S'
-};
+const countries = [
+    {
+        country: '0',
+        city: 'Choose a city'
+    },
+    {
+        country: 'arg',
+        flag: 'flags/arg.png',
+        airportCode: 'EZE',
+        airportName: 'Ezeiza Airport',
+        city: 'Buenos Aires, Argentina'
+    },
+    {
+        country: 'aus',
+        flag: 'flags/aus.png',
+        airportCode: 'SYD',
+        airportName: 'Sydney Kingsford Smith Airport',
+        city: 'Sydney, Australia'
+    },
+    {
+        country: 'aut',
+        flag: 'flags/aut.png',
+        airportCode: 'VIE',
+        airportName: 'Vienna Airport',
+        city: 'Vienna, Austria'
+    },
+    {
+        country: 'bel',
+        flag: 'flags/bel.png',
+        airportCode: 'BRU',
+        airportName: 'Brussels Airport',
+        city: 'Brussels, Belgium'
+    },
+    {
+        country: 'bol',
+        flag: 'flags/bol.png',
+        airportCode: 'VVI',
+        airportName: 'Viru Viru Airport',
+        city: 'Santa Cruz de la Sierra, Bolivia'
+    },
+    {
+        country: 'bra',
+        flag: 'flags/bra.png',
+        airportCode: 'GRU',
+        airportName: 'Guarulhos Airport',
+        city: 'São Paulo, Brazil'
+    },
+    {
+        country: 'bwa',
+        flag: 'flags/bwa.png',
+        airportCode: 'GBE',
+        airportName: 'Sir Seretse Khama Airport',
+        city: 'Gaborone, Botswana'
+    },
+    {
+        country: 'can',
+        flag: 'flags/can.png',
+        airportCode: 'YYZ',
+        airportName: 'Lester B. Pearson Airport',
+        city: 'Ontario, Canada'
+    },
+    {
+        country: 'chl',
+        flag: 'flags/chl.png',
+        airportCode: 'SCL',
+        airportName: 'Arturo Merino Benítez Airport',
+        city: 'Santiago, Chile'
+    },
+    {
+        country: 'chn',
+        flag: 'flags/chn.png',
+        airportCode: 'PEK',
+        airportName: 'Beijing Capital Airport',
+        city: 'Beijing, China'
+    },
+    {
+        country: 'cym',
+        flag: 'flags/cym.png',
+        airportCode: 'GCM',
+        airportName: 'Owen Roberts Airport',
+        city: 'George Town, Cayman Islands'
+    },
+    {
+        country: 'cze',
+        flag: 'flags/cze.png',
+        airportCode: 'PRG',
+        airportName: 'Václav Havel Airport',
+        city: 'Prague, Czech Republic'
+    },
+    {
+        country: 'deu',
+        flag: 'flags/deu.png',
+        airportCode: 'FRA',
+        airportName: 'Frankfurt Airport',
+        city: 'Frankfurt, Germany'
+    },
+    {
+        country: 'eng',
+        flag: 'flags/eng.png',
+        airportCode: 'LHR',
+        airportName: 'Heathrow Airport',
+        city: 'London, England, UK'
+    },
+    {
+        country: 'fra',
+        flag: 'flags/fra.png',
+        airportCode: 'CDG',
+        airportName: 'Paris Charles de Gaulle Airport',
+        city: 'Paris, France'
+    },
+    {
+        country: 'idn',
+        flag: 'flags/idn.png',
+        airportCode: 'CGK',
+        airportName: 'Soekarno–Hatta Airport',
+        city: 'Jakarta, Indonesia'
+    },
+    {
+        country: 'irl',
+        flag: 'flags/irl.png',
+        airportCode: 'DUB',
+        airportName: 'Dublin Airport',
+        city: 'Dublin, Ireland'
+    },
+    {
+        country: 'jpn',
+        flag: 'flags/jpn.png',
+        airportCode: 'NRT',
+        airportName: 'Narita Airport',
+        city: 'Narita, Chiba, Japan'
+    },
+    {
+        country: 'kor',
+        flag: 'flags/kor.png',
+        airportCode: 'ICN',
+        airportName: 'Incheon Airport',
+        city: 'Jung District, South Korea'
+    },
+    {
+        country: 'nzl',
+        flag: 'flags/nzl.png',
+        airportCode: 'AKL',
+        airportName: 'Auckland Airport',
+        city: 'Auckland, New Zealand'
+    },
+    {
+        country: 'pri',
+        flag: 'flags/pri.png',
+        airportCode: 'SJU',
+        airportName: 'Luis Muñoz Marín Airport',
+        city: 'San Juan, Puerto Rico'
+    },
+    {
+        country: 'sgp',
+        flag: 'flags/sgp.png',
+        airportCode: 'SIN',
+        airportName: 'Changi Airport',
+        city: 'Changi, Singapore'
+    },
+    {
+        country: 'swe',
+        flag: 'flags/swe.png',
+        airportCode: 'ARN',
+        airportName: 'Stockholm Arlanda Airport',
+        city: 'Stockholm, Sweden'
+    },
+    {
+        country: 'twn',
+        flag: 'flags/twn.png',
+        airportCode: 'TPE',
+        airportName: 'Taiwan Taoyuan Airport',
+        city: 'Dayuan District, Taiwan'
+    },
+    {
+        country: 'usa',
+        flag: 'flags/usa.png',
+        airportCode: 'JFK',
+        airportName: 'John F. Kennedy Airport',
+        city: 'New York, U.S'
+    }
+];
--- script.js
+++ script.js
@@ -1,138 +1,45 @@
+// date info
 const date = new Date().toLocaleDateString();
 document.getElementById("todayDate").innerHTML = `<h5>${date}</h5>`;
 
-// first select
-function update1(id){
-    let select1 = document.getElementById("flyingFrom");
-    let value1 = select1.options[select1.selectedIndex].value;
-    console.log(value1);
 
-    switch (value1) {
-        case 'ARG':
-        document.getElementById(id).innerHTML = `<img src='${arg.flag}'> <h3>${arg.airportCode}</h3> <p>${arg.airportName}</p> <p>${arg.city}</p>`;
-        break;
-        case 'AUS':
-        document.getElementById(id).innerHTML = `<img src='${aus.flag}'> <h3>${aus.airportCode}</h3> <p>${aus.airportName}</p> <p>${aus.city}</p>`;
-        break;
-        case 'AUT':
-        document.getElementById(id).innerHTML = `<img src='${aut.flag}'> <h3>${aut.airportCode}</h3> <p>${aut.airportName}</p> <p>${aut.city}</p>`;
-        break;
-        case 'BEL':
-        document.getElementById(id).innerHTML = `<img src='${bel.flag}'> <h3>${bel.airportCode}</h3> <p>${bel.airportName}</p> <p>${bel.city}</p>`;
-        break;
-        case 'BOL':
-        document.getElementById(id).innerHTML = `<img src='${bol.flag}'> <h3>${bol.airportCode}</h3> <p>${bol.airportName}</p> <p>${bol.city}</p>`;
-        break;
-        case 'BRA':
-        document.getElementById(id).innerHTML = `<img src='${bra.flag}'> <h3>${bra.airportCode}</h3> <p>${bra.airportName}</p> <p>${bra.city}</p>`;
-        break;
-        case 'BWA':
-        document.getElementById(id).innerHTML = `<img src='${bwa.flag}'> <h3>${bwa.airportCode}</h3> <p>${bwa.airportName}</p> <p>${bwa.city}</p>`;
-        break;
-        case 'CAN':
-        document.getElementById(id).innerHTML = `<img src='${can.flag}'> <h3>${can.airportCode}</h3> <p>${can.airportName}</p> <p>${can.city}</p>`;
-        break;
-        case 'CHL':
-        document.getElementById(id).innerHTML = `<img src='${chl.flag}'> <h3>${chl.airportCode}</h3> <p>${chl.airportName}</p> <p>${chl.city}</p>`;
-        break;
-        case 'CHN':
-        document.getElementById(id).innerHTML = `<img src='${chn.flag}'> <h3>${chn.airportCode}</h3> <p>${chn.airportName}</p> <p>${chn.city}</p>`;
-        break;
-        case 'CYM':
-        document.getElementById(id).innerHTML = `<img src='${cym.flag}'> <h3>${cym.airportCode}</h3> <p>${cym.airportName}</p> <p>${cym.city}</p>`;
-        break;
-        case 'CZE':
-        document.getElementById(id).innerHTML = `<img src='${cze.flag}'> <h3>${cze.airportCode}</h3> <p>${cze.airportName}</p> <p>${cze.city}</p>`;
-        break;
-        case 'ENG':
-        document.getElementById(id).innerHTML = `<img src='${eng.flag}'> <h3>${eng.airportCode}</h3> <p>${eng.airportName}</p> <p>${eng.city}</p>`;
-        break;
-        case 'IDN':
-        document.getElementById(id).innerHTML = `<img src='${idn.flag}'> <h3>${idn.airportCode}</h3> <p>${idn.airportName}</p> <p>${idn.city}</p>`;
-        break;
-        case 'JPN':
-        document.getElementById(id).innerHTML = `<img src='${jpn.flag}'> <h3>${jpn.airportCode}</h3> <p>${jpn.airportName}</p> <p>${jpn.city}</p>`;
-        break;
-        case 'KOR':
-        document.getElementById(id).innerHTML = `<img src='${kor.flag}'> <h3>${kor.airportCode}</h3> <p>${kor.airportName}</p> <p>${kor.city}</p>`;
-        break;
-        case 'NZL':
-        document.getElementById(id).innerHTML = `<img src='${nzl.flag}'> <h3>${nzl.airportCode}</h3> <p>${nzl.airportName}</p> <p>${nzl.city}</p>`;
-        break;
-        case 'SWE':
-        document.getElementById(id).innerHTML = `<img src='${swe.flag}'> <h3>${swe.airportCode}</h3> <p>${swe.airportName}</p> <p>${swe.city}</p>`;
-        break;
-        case 'USA':
-        document.getElementById(id).innerHTML = `<img src='${usa.flag}'> <h3>${usa.airportCode}</h3> <p>${usa.airportName}</p> <p>${usa.city}</p>`;
-        break;
+// function to catch the objects value
+function optionLoop(nameSpan) {
+    let i = 0;
+    let allCities = "";
+    let target = document.querySelector(nameSpan);
+    
+    if (!target) {
+      console.error("Element not found: " + nameSpan);
+      return;
     }
+    
+    // loop to the end of the array and format the options value
+    for (; i < countries.length; i++) {
+      allCities += `<option value="${countries[i].country}">${countries[i].city}</option>`;
+    }
+    
+    target.innerHTML = allCities;
 };
+optionLoop("#flyingFrom");
+optionLoop("#flyingTo");
 
-// second select
-function update2(id){
-    let select2 = document.getElementById("flyingTo");
-    let value2 = select2.options[select2.selectedIndex].value;
-    console.log(value2);
 
-    switch (value2) {
-        case 'ARG':
-        document.getElementById(id).innerHTML = `<img src='${arg.flag}'> <h3>${arg.airportCode}</h3> <p>${arg.airportName}</p> <p>${arg.city}</p>`;
-        break;
-        case 'AUS':
-        document.getElementById(id).innerHTML = `<img src='${aus.flag}'> <h3>${aus.airportCode}</h3> <p>${aus.airportName}</p> <p>${aus.city}</p>`;
-        break;
-        case 'AUT':
-        document.getElementById(id).innerHTML = `<img src='${aut.flag}'> <h3>${aut.airportCode}</h3> <p>${aut.airportName}</p> <p>${aut.city}</p>`;
-        break;
-        case 'BEL':
-        document.getElementById(id).innerHTML = `<img src='${bel.flag}'> <h3>${bel.airportCode}</h3> <p>${bel.airportName}</p> <p>${bel.city}</p>`;
-        break;
-        case 'BOL':
-        document.getElementById(id).innerHTML = `<img src='${bol.flag}'> <h3>${bol.airportCode}</h3> <p>${bol.airportName}</p> <p>${bol.city}</p>`;
-        break;
-        case 'BRA':
-        document.getElementById(id).innerHTML = `<img src='${bra.flag}'> <h3>${bra.airportCode}</h3> <p>${bra.airportName}</p> <p>${bra.city}</p>`;
-        break;
-        case 'BWA':
-        document.getElementById(id).innerHTML = `<img src='${bwa.flag}'> <h3>${bwa.airportCode}</h3> <p>${bwa.airportName}</p> <p>${bwa.city}</p>`;
-        break;
-        case 'CAN':
-        document.getElementById(id).innerHTML = `<img src='${can.flag}'> <h3>${can.airportCode}</h3> <p>${can.airportName}</p> <p>${can.city}</p>`;
-        break;
-        case 'CHL':
-        document.getElementById(id).innerHTML = `<img src='${chl.flag}'> <h3>${chl.airportCode}</h3> <p>${chl.airportName}</p> <p>${chl.city}</p>`;
-        break;
-        case 'CHN':
-        document.getElementById(id).innerHTML = `<img src='${chn.flag}'> <h3>${chn.airportCode}</h3> <p>${chn.airportName}</p> <p>${chn.city}</p>`;
-        break;
-        case 'CYM':
-        document.getElementById(id).innerHTML = `<img src='${cym.flag}'> <h3>${cym.airportCode}</h3> <p>${cym.airportName}</p> <p>${cym.city}</p>`;
-        break;
-        case 'CZE':
-        document.getElementById(id).innerHTML = `<img src='${cze.flag}'> <h3>${cze.airportCode}</h3> <p>${cze.airportName}</p> <p>${cze.city}</p>`;
-        break;
-        case 'ENG':
-        document.getElementById(id).innerHTML = `<img src='${eng.flag}'> <h3>${eng.airportCode}</h3> <p>${eng.airportName}</p> <p>${eng.city}</p>`;
-        break;
-        case 'IDN':
-        document.getElementById(id).innerHTML = `<img src='${idn.flag}'> <h3>${idn.airportCode}</h3> <p>${idn.airportName}</p> <p>${idn.city}</p>`;
-        break;
-        case 'JPN':
-        document.getElementById(id).innerHTML = `<img src='${jpn.flag}'> <h3>${jpn.airportCode}</h3> <p>${jpn.airportName}</p> <p>${jpn.city}</p>`;
-        break;
-        case 'KOR':
-        document.getElementById(id).innerHTML = `<img src='${kor.flag}'> <h3>${kor.airportCode}</h3> <p>${kor.airportName}</p> <p>${kor.city}</p>`;
-        break;
-        case 'NZL':
-        document.getElementById(id).innerHTML = `<img src='${nzl.flag}'> <h3>${nzl.airportCode}</h3> <p>${nzl.airportName}</p> <p>${nzl.city}</p>`;
-        break;
-        case 'SWE':
-        document.getElementById(id).innerHTML = `<img src='${swe.flag}'> <h3>${swe.airportCode}</h3> <p>${swe.airportName}</p> <p>${swe.city}</p>`;
-        break;
-        case 'USA':
-        document.getElementById(id).innerHTML = `<img src='${usa.flag}'> <h3>${usa.airportCode}</h3> <p>${usa.airportName}</p> <p>${usa.city}</p>`;
-        break;
-    }
+// function with filter, needs ID from select and ID from span, return selected value for each select.
+function update(id, spanID) {
+    // take the value
+    let select = document.getElementById(id);
+    let selectValue = select.options[select.selectedIndex].value;
+    console.log(selectValue);
+    
+    // filter the value
+    const selectFilter = countries.filter(
+        (country) => country.country === selectValue
+    );
+    console.log(selectFilter[0].city);
+
+    // formats the object returned in span, country info
+    document.getElementById(spanID).innerHTML = `<img src='${selectFilter[0].flag}'> <h3>${selectFilter[0].airportCode}</h3> <p>${selectFilter[0].airportName}</p> <p>${selectFilter[0].city}</p>`;
 };
 
 // multicity? yes. then...
@@ -146,32 +53,11 @@ function multiCityOption() {
         <div class="local-date">
             <div class="local">
                 <div class="flying">
-                    <select name="flyingMultiCity" id="flyingMultiCity" onchange="update3('flyingMultiCityData')">
-                        <option value="0" selected>Choose a city</option>
-                        <option value="ARG">Buenos Aires, Argentina</option>
-                        <option value="AUS">Sydney, Australia</option>
-                        <option value="AUT">Vienna, Austria</option>
-                        <option value="BEL">Brussels, Belgium</option>
-                        <option value="BOL">Santa Cruz de la Sierra, Bolivia</option>
-                        <option value="BRA">São Paulo, Brazil</option>
-                        <option value="BWA">Gaborone, Botswana</option>
-                        <option value="CAN">Ontario, Canada</option>
-                        <option value="CHL">Santiago, Chile</option>
-                        <option value="CHN">Beijing, China</option>
-                        <option value="CYM">George Town, Cayman Islands</option>
-                        <option value="CZE">Prague, Czech Republic</option>
-                        <option value="ENG">London, England, UK</option>
-                        <option value="IDN">Jakarta, Indonesia</option>
-                        <option value="JPN">Narita, Japan</option>
-                        <option value="KOR">Jung District, South Korea</option>
-                        <option value="NZL">Auckland, New Zealand</option>
-                        <option value="SWE">Stockholm, Sweden</option>
-                        <option value="USA">New York, U.S</option>
-                    </select>
+                    <select name="flyingMultiCity" id="flyingMultiCity" onchange="update('flyingMultiCity', 'flyingMultiCitySpan')"></select>
                 </div>
 
                 <div class="flyingMultiCity">
-                    <span id="flyingMultiCityData"></span>
+                    <span id="flyingMultiCitySpan"></span>
                 </div>
             </div>
 
@@ -190,71 +76,7 @@ function multiCityOption() {
               </div>
             </div>  
         </div>`
+        optionLoop("#flyingMultiCity");
     }
 };
 multiCityOption();
-
-function update3(id){
-    const value3 = document.querySelector('select[id="flyingMultiCity"]').value;
-    console.log(value3);
-
-    switch (value3) {
-        case 'ARG':
-        document.getElementById(id).innerHTML = `<img src='${arg.flag}'> <h3>${arg.airportCode}</h3> <p>${arg.airportName}</p> <p>${arg.city}</p>`;
-        break;
-        case 'AUS':
-        document.getElementById(id).innerHTML = `<img src='${aus.flag}'> <h3>${aus.airportCode}</h3> <p>${aus.airportName}</p> <p>${aus.city}</p>`;
-        break;
-        case 'AUT':
-        document.getElementById(id).innerHTML = `<img src='${aut.flag}'> <h3>${aut.airportCode}</h3> <p>${aut.airportName}</p> <p>${aut.city}</p>`;
-        break;
-        case 'BEL':
-        document.getElementById(id).innerHTML = `<img src='${bel.flag}'> <h3>${bel.airportCode}</h3> <p>${bel.airportName}</p> <p>${bel.city}</p>`;
-        break;
-        case 'BOL':
-        document.getElementById(id).innerHTML = `<img src='${bol.flag}'> <h3>${bol.airportCode}</h3> <p>${bol.airportName}</p> <p>${bol.city}</p>`;
-        break;
-        case 'BRA':
-        document.getElementById(id).innerHTML = `<img src='${bra.flag}'> <h3>${bra.airportCode}</h3> <p>${bra.airportName}</p> <p>${bra.city}</p>`;
-        break;
-        case 'BWA':
-        document.getElementById(id).innerHTML = `<img src='${bwa.flag}'> <h3>${bwa.airportCode}</h3> <p>${bwa.airportName}</p> <p>${bwa.city}</p>`;
-        break;
-        case 'CAN':
-        document.getElementById(id).innerHTML = `<img src='${can.flag}'> <h3>${can.airportCode}</h3> <p>${can.airportName}</p> <p>${can.city}</p>`;
-        break;
-        case 'CHL':
-        document.getElementById(id).innerHTML = `<img src='${chl.flag}'> <h3>${chl.airportCode}</h3> <p>${chl.airportName}</p> <p>${chl.city}</p>`;
-        break;
-        case 'CHN':
-        document.getElementById(id).innerHTML = `<img src='${chn.flag}'> <h3>${chn.airportCode}</h3> <p>${chn.airportName}</p> <p>${chn.city}</p>`;
-        break;
-        case 'CYM':
-        document.getElementById(id).innerHTML = `<img src='${cym.flag}'> <h3>${cym.airportCode}</h3> <p>${cym.airportName}</p> <p>${cym.city}</p>`;
-        break;
-        case 'CZE':
-        document.getElementById(id).innerHTML = `<img src='${cze.flag}'> <h3>${cze.airportCode}</h3> <p>${cze.airportName}</p> <p>${cze.city}</p>`;
-        break;
-        case 'ENG':
-        document.getElementById(id).innerHTML = `<img src='${eng.flag}'> <h3>${eng.airportCode}</h3> <p>${eng.airportName}</p> <p>${eng.city}</p>`;
-        break;
-        case 'IDN':
-        document.getElementById(id).innerHTML = `<img src='${idn.flag}'> <h3>${idn.airportCode}</h3> <p>${idn.airportName}</p> <p>${idn.city}</p>`;
-        break;
-        case 'JPN':
-        document.getElementById(id).innerHTML = `<img src='${jpn.flag}'> <h3>${jpn.airportCode}</h3> <p>${jpn.airportName}</p> <p>${jpn.city}</p>`;
-        break;
-        case 'KOR':
-        document.getElementById(id).innerHTML = `<img src='${kor.flag}'> <h3>${kor.airportCode}</h3> <p>${kor.airportName}</p> <p>${kor.city}</p>`;
-        break;
-        case 'NZL':
-        document.getElementById(id).innerHTML = `<img src='${nzl.flag}'> <h3>${nzl.airportCode}</h3> <p>${nzl.airportName}</p> <p>${nzl.city}</p>`;
-        break;
-        case 'SWE':
-        document.getElementById(id).innerHTML = `<img src='${swe.flag}'> <h3>${swe.airportCode}</h3> <p>${swe.airportName}</p> <p>${swe.city}</p>`;
-        break;
-        case 'USA':
-        document.getElementById(id).innerHTML = `<img src='${usa.flag}'> <h3>${usa.airportCode}</h3> <p>${usa.airportName}</p> <p>${usa.city}</p>`;
-        break;
-    }
-};
